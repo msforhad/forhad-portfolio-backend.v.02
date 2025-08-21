6a17107466c509b2da8e6a0b76449cacd0a9800e
msforhad <msforhad717@gmail.com>
2025-08-21 18:13:18 +0600

    6

--- api/index.js
+++ api/index.js
@@ -2,7 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import serverless from 'serverless-http';
-import sendMessageRouter from '../routes/sendMessage.js';
+import transporter from '../config/nodemailer.js';
+import { contactEmailHTML, contactEmailTEXT } from '../utils/emailTemplate.js';
+import { autoReplyHTML, autoReplyTEXT } from '../utils/autoReplyTemplate.js';
 
 const allowedOrigins = [ 
   'https://forhad-portfolio-five.vercel.app'
@@ -30,7 +32,45 @@ app.use(
 )
 
 // ✅ API routes
-app.use('/api/contact', sendMessageRouter)
+app.post('/api/contact/send-message',async(req,res)=>{
+  const {name,email,phone,message}=req.body;
+      if(!name||!email||!phone||!message){
+      return res.json({success:false,message:'All fields are required!'})
+    }
+
+  try {
+
+    await transporter.sendMail({
+      
+      from:`"FO|RH|AD Contact" <${process.env.SENDER_EMAIL}>`,
+      to:process.env.SENDER_EMAIL,
+      subject:`New Message from ${name}`,
+      html: contactEmailHTML({ name, email, phone, message, brandName: "FO|RH|AD",}),
+      text: contactEmailTEXT({ name, email, phone, message, brandName: "FO|RH|AD" }),
+      replyTo: email,
+      headers: { "X-Entity-Ref-ID": Date.now().toString() },
+      priority: "high"
+
+    })
+
+    // 2️⃣ Auto-reply to client
+    await transporter.sendMail({
+      from: `"FO|RH|AD" <${process.env.SENDER_EMAIL}>`,
+      to: email,
+      subject: `Thank you for contacting FO|RH|AD!`,
+      html: autoReplyHTML({ name, brandName: "FO|RH|AD",}),
+      text: autoReplyTEXT({ name, brandName: "FO|RH|AD" })
+    });
+
+
+  
+    return res.json({success:true,message:' Your message has been successfully sent!'})
+    
+  } catch (error) {
+    return res.json({success:false,message:"Failed to send message. Try again later."})
+    
+  }
+})
 app.get('/api', (req,res) => {
   res.json("Forhad portfolio backend server running 🚀")
 })
--- routes/sendMessage.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { sendMessageController } from '../controller/sendMessageController.js';
-
-
-const sendMessageRouter = express.Router();
-
-sendMessageRouter.post('/send-message',sendMessageController)
-
-export default sendMessageRouter
